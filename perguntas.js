0e647aad51ab8e720b5575173ad516640d46a7b6
abner315 <00001108290838sp@al.educacao.sp.gov.br>
2024-12-04 10:42:17 -0300

    first version

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil?',
+    'A capital da Brasil é Brasília'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua espanhola',
+    'Como se diz oi espanhol?',
+    'Oi em ingles é HOLA'
+)
